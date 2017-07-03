8cc8e547a6b8268ddb4e9bafbf8f1bd2f5e18948
LEONARDO VENANCIO CORREIA <leonardovenancio12@gmail.com>
2017-07-03 20:28:44 -0300

    Add files via upload

--- AlgMono.py
+++ AlgMono.py
@@ -4,6 +4,9 @@ Created on Wed Jun 07 18:18:50 2017
 
 @author: Leonardo Venancio - Monografia
 """
+from math import sqrt
+
+"""
 print "Para posições positivas\n"
 
 R1 = float(raw_input('Posicao Satelite: '))
@@ -44,4 +47,80 @@ elif h2==h4:
 elif h3==h4:
     print 'h = '+ str(h3)+' m'
 
-    
+"""
+print "Teste Satélite 3D\n"
+#Satélites
+#1
+#####################################################################
+X1 = float(raw_input('Digite a posicao X do primeiro satelite: '))
+Y1 = float(raw_input('Digite a posicao Y do primeiro satelite: '))
+Z1 = float(raw_input('Digite a posicao Z do primeiro satelite: '))
+#2
+#####################################################################
+X2 = float(raw_input('Digite a posicao X do segundo satelite: '))
+Y2 = float(raw_input('Digite a posicao Y do segundo satelite: '))
+Z2 = float(raw_input('Digite a posicao Z do segundo satelite: '))
+#3
+#####################################################################
+X3 = float(raw_input('Digite a posicao X do terceiro satelite: '))
+Y3 = float(raw_input('Digite a posicao Y do terceiro satelite: '))
+Z3 = float(raw_input('Digite a posicao Z do terceiro satelite: '))
+#4
+#####################################################################
+X4 = float(raw_input('Digite a posicao X do quarto satelite: '))
+Y4 = float(raw_input('Digite a posicao Y do quarto satelite: '))
+Z4 = float(raw_input('Digite a posicao Z do qaurto satelite: '))
+#####################################################################
+
+#Distancia
+D1 = 1
+D2 = 1
+D3 = 1
+D4 = 1
+
+X =(-0.5*((D1**2)*Y2*Z3 - (D1**2)*Y2*Z4 - (D1**2)*Y3*Z2 + (D1**2)*Y3*Z4 + (D1**2)*Y4*Z2 - 
+          (D1**2)*Y4*Z3 - (D2**2)*Y1*Z3 + (D2**2)*Y1*Z4 + (D2**2)*Y3*Z1 - (D2**2)*Y3*Z4 - 
+          (D2**2)*Y4*Z1 + (D2**2)*Y4*Z3 + (D3**2)*Y1*Z2 - (D3**2)*Y1*Z4 - (D3**2)*Y2*Z1 +
+          (D3**2)*Y2*Z4 + (D3**2)*Y4*Z1 - (D3**2)*Y4*Z2 - (D4**2)*Y1*Z2 + (D4**2)*Y1*Z3 +
+          (D4**2)*Y2*Z1 - (D4**2)*Y2*Z3 - (D4**2)*Y3*Z1 + (D4**2)*Y3*Z2 - (X1**2)*Y2*Z3 +
+          (X1**2)*Y2*Z4 + (X1**2)*Y3*Z2 - (X1**2)*Y3*Z4 - (X1**2)*Y4*Z2 + (X1**2)*Y4*Z3 +
+          (X2**2)*Y1*Z3 - (X2**2)*Y1*Z4 - (X2**2)*Y3*Z1 + (X2**2)*Y3*Z4 + (X2**2)*Y4*Z1 -
+          (X2**2)*Y4*Z3 - (X3**2)*Y1*Z2 + (X3**2)*Y1*Z4 + (X3**2)*Y2*Z1 - (X3**2)*Y2*Z4 -
+          (X3**2)*Y4*Z1 + (X3**2)*Y4*Z2 + (X4**2)*Y1*Z2 - (X4**2)*Y1*Z3 - (X4**2)*Y2*Z1 +
+          (X4**2)*Y2*Z3 + (X4**2)*Y3*Z1 - (X4**2)*Y3*Z2 - (Y1**2)*Y2*Z3 + (Y1**2)*Y2*Z4 +
+          (Y1**2)*Y3*Z2 - (Y1**2)*Y3*Z4 - (Y1**2)*Y4*Z2 + (Y1**2)*Y4*Z3 + (Y2**2)*Y1*Z3 -
+          (Y2**2)*Y1*Z4 - (Y3**2)*Y1*Z2 + (Y3**2)*Y1*Z4 + (Y4**2)*Y1*Z2 - (Y4**2)*Y1*Z3 +
+          (Z2**2)*Y1*Z3 - (Z2**2)*Y1*Z4 - (Z3**2)*Z2*Y1 + (Z4**2)*Y1*Z2 + (Z3**2)*Y1*Z4 -
+          (Z4**2)*Z3*Y1 - (Y2**2)*Y3*Z1 + (Y2**2)*Y3*Z4 + (Y2**2)*Y4*Z1 - (Y2**2)*Y4*Z3 +
+          (Y3**2)*Y3*Z1 - (Y3**2)*Y2*Z4 - (Y4**2)*Y2*Z1 + (Y4**2)*Y2*Z3 - (Z1**2)*Y2*Z3 +
+          (Z1**2)*Y2*Z4 + (Z3**2)*Z1*Y2 - (Z4**2)*Z1*Y2 - (Z3**2)*Z4*Y2 + (Z4**2)*Z3*Y2 -
+          (Y3**2)*Y4*Z1 + (Y3**2)*Y4*Z2 + (Y4**2)*Y3*Z1 - (Y4**2)*Y3*Z2 + (Z1**2)*Z2*Y3 -
+          (Z1**2)*Z4*Y3 - (Z2**2)*Z1*Y3 + (Z4**2)*Z1*Y3 + (Z2**2)*Z4*Y3 - (Z4**2)*Z2*Y3 -
+          (Z1**2)*Z2*Y4 + (Z1**2)*Z3*Y4 + (Z2**2)*Z1*Y4 - (Z3**2)*Z1*Y4 - (Z2**2)*Z3*Y4 +
+          (Z3**2)*Z2*Y4)/(X1*Y2*Z3 - X1*Y2*Z4 - X1*Y3*Z2 + X1*Y3*Z4 + X1*Y4*Z2 - 
+          X1*Y4*Z3 - X2*Y1*Z3 + X2*Y1*Z4 + X2*Y3*Z1 - X2*Y3*Z4 - X2*Y4*Z1 + X2*Y4*Z3 + 
+          X3*Y1*Z2 - X3*Y1*Z4 - X3*Y2*Z1 + X3*Y2*Z4 + X3*Y4*Z1 - X3*Y4*Z2 - X4*Y1*Z2 +
+          X4*Y1*Z3 + X4*Y2*Z1 - X4*Y2*Z3 - X4*Y3*Z1 + X4*Y3*Z2))
+Y= ()
+Z= ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
