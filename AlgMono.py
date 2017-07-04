93fb704129bdc6a1e865931a2338db19a2b5ecfc
LEONARDO VENANCIO CORREIA <leonardovenancio12@gmail.com>
2017-07-04 15:16:49 -0300

    Add files via upload

--- AlgMono.py
+++ AlgMono.py
@@ -4,7 +4,7 @@ Created on Wed Jun 07 18:18:50 2017
 
 @author: Leonardo Venancio - Monografia
 """
-from math import sqrt
+#from math import sqrt
 
 """
 print "Para posições positivas\n"
@@ -49,6 +49,16 @@ elif h3==h4:
 
 """
 print "Teste Satélite 3D\n"
+
+c = 299792458 #velocidade da Luz
+
+#tempos
+
+t1 = 0.00505783
+t2 = 0.00423206
+t3 = 0.00836090
+t4 = 0.00712225
+
 #Satélites
 #1
 #####################################################################
@@ -71,41 +81,67 @@ X4 = float(raw_input('Digite a posicao X do quarto satelite: '))
 Y4 = float(raw_input('Digite a posicao Y do quarto satelite: '))
 Z4 = float(raw_input('Digite a posicao Z do qaurto satelite: '))
 #####################################################################
+V2x = X1 - X2
+V3x = X1 - X3
+V4x = X1 - X4
+V2y = Y1 - Y2
+V3y = Y1 - Y3
+V4y = Y1 - Y4
+V2z = Z1 - Z2
+V3z = Z1 - Z3
+V4z = Z1 - Z4
 
 #Distancia
-D1 = 1
-D2 = 1
-D3 = 1
-D4 = 1
+D1 = c*t1
+D2 = c*t2
+D3 = c*t3
+D4 = c*t4
 
-X =(-0.5*((D1**2)*Y2*Z3 - (D1**2)*Y2*Z4 - (D1**2)*Y3*Z2 + (D1**2)*Y3*Z4 + (D1**2)*Y4*Z2 - 
-          (D1**2)*Y4*Z3 - (D2**2)*Y1*Z3 + (D2**2)*Y1*Z4 + (D2**2)*Y3*Z1 - (D2**2)*Y3*Z4 - 
-          (D2**2)*Y4*Z1 + (D2**2)*Y4*Z3 + (D3**2)*Y1*Z2 - (D3**2)*Y1*Z4 - (D3**2)*Y2*Z1 +
-          (D3**2)*Y2*Z4 + (D3**2)*Y4*Z1 - (D3**2)*Y4*Z2 - (D4**2)*Y1*Z2 + (D4**2)*Y1*Z3 +
-          (D4**2)*Y2*Z1 - (D4**2)*Y2*Z3 - (D4**2)*Y3*Z1 + (D4**2)*Y3*Z2 - (X1**2)*Y2*Z3 +
-          (X1**2)*Y2*Z4 + (X1**2)*Y3*Z2 - (X1**2)*Y3*Z4 - (X1**2)*Y4*Z2 + (X1**2)*Y4*Z3 +
-          (X2**2)*Y1*Z3 - (X2**2)*Y1*Z4 - (X2**2)*Y3*Z1 + (X2**2)*Y3*Z4 + (X2**2)*Y4*Z1 -
-          (X2**2)*Y4*Z3 - (X3**2)*Y1*Z2 + (X3**2)*Y1*Z4 + (X3**2)*Y2*Z1 - (X3**2)*Y2*Z4 -
-          (X3**2)*Y4*Z1 + (X3**2)*Y4*Z2 + (X4**2)*Y1*Z2 - (X4**2)*Y1*Z3 - (X4**2)*Y2*Z1 +
-          (X4**2)*Y2*Z3 + (X4**2)*Y3*Z1 - (X4**2)*Y3*Z2 - (Y1**2)*Y2*Z3 + (Y1**2)*Y2*Z4 +
-          (Y1**2)*Y3*Z2 - (Y1**2)*Y3*Z4 - (Y1**2)*Y4*Z2 + (Y1**2)*Y4*Z3 + (Y2**2)*Y1*Z3 -
-          (Y2**2)*Y1*Z4 - (Y3**2)*Y1*Z2 + (Y3**2)*Y1*Z4 + (Y4**2)*Y1*Z2 - (Y4**2)*Y1*Z3 +
-          (Z2**2)*Y1*Z3 - (Z2**2)*Y1*Z4 - (Z3**2)*Z2*Y1 + (Z4**2)*Y1*Z2 + (Z3**2)*Y1*Z4 -
-          (Z4**2)*Z3*Y1 - (Y2**2)*Y3*Z1 + (Y2**2)*Y3*Z4 + (Y2**2)*Y4*Z1 - (Y2**2)*Y4*Z3 +
-          (Y3**2)*Y2*Z1 - (Y3**2)*Y2*Z4 - (Y4**2)*Y2*Z1 + (Y4**2)*Y2*Z3 - (Z1**2)*Y2*Z3 +
-          (Z1**2)*Y2*Z4 + (Z3**2)*Z1*Y2 - (Z4**2)*Z1*Y2 - (Z3**2)*Z4*Y2 + (Z4**2)*Z3*Y2 -
-          (Y3**2)*Y4*Z1 + (Y3**2)*Y4*Z2 + (Y4**2)*Y3*Z1 - (Y4**2)*Y3*Z2 + (Z1**2)*Z2*Y3 -
-          (Z1**2)*Z4*Y3 - (Z2**2)*Z1*Y3 + (Z4**2)*Z1*Y3 + (Z2**2)*Z4*Y3 - (Z4**2)*Z2*Y3 -
-          (Z1**2)*Z2*Y4 + (Z1**2)*Z3*Y4 + (Z2**2)*Z1*Y4 - (Z3**2)*Z1*Y4 - (Z2**2)*Z3*Y4 +
-          (Z3**2)*Z2*Y4)/(X1*Y2*Z3 - X1*Y2*Z4 - X1*Y3*Z2 + X1*Y3*Z4 + X1*Y4*Z2 - 
-          X1*Y4*Z3 - X2*Y1*Z3 + X2*Y1*Z4 + X2*Y3*Z1 - X2*Y3*Z4 - X2*Y4*Z1 + X2*Y4*Z3 + 
-          X3*Y1*Z2 - X3*Y1*Z4 - X3*Y2*Z1 + X3*Y2*Z4 + X3*Y4*Z1 - X3*Y4*Z2 - X4*Y1*Z2 +
-          X4*Y1*Z3 + X4*Y2*Z1 - X4*Y2*Z3 - X4*Y3*Z1 + X4*Y3*Z2))
-Y= ()
-Z= ()
+X = (-0.5*(D1**2*V2y*V3z-D1**2*V2y*V4z-D1**2*V2z*V3y+D1**2*V2z*V4y
+           +D1**2*V3y*V4z-D1**2*V3z*V4y-D2**2*V3y*V4z+D2**2*V3z*V4y
+           +D3**2*V2y*V4z-D3**2*V2z*V4y-D4**2*V2y*V3z+D4**2*V2z*V3y
+           -V2y*V3z*X1**2+V2y*V3z*X4**2-V2y*V3z*Y1**2+V2y*V3z*Y4**2
+           -V2y*V3z*Z1**2+V2y*V3z*Z4**2+V2y*V4z*X1**2-V2y*V4z*X3**2
+           +V2y*V4z*Y1**2-V2y*V4z*Y3**2+V2y*V4z*Z1**2-V2y*V4z*Z3**2
+           +V2z*V3y*X1**2-V2z*V3y*X4**2+V2z*V3y*Y1**2-V2z*V3y*Y4**2
+           +V2z*V3y*Z1**2-V2z*V3y*Z4**2-V2z*V4y*X1**2+V2z*V4y*X3**2
+           -V2z*V4y*Y1**2+V2z*V4y*Y3**2-V2z*V4y*Z1**2+V2z*V4y*Z3**2
+           -V3y*V4z*X1**2+V3y*V4z*X2**2-V3y*V4z*Y1**2+V3y*V4z*Y2**2
+           -V3y*V4z*Z1**2+V3y*V4z*Z2**2+V3z*V4y*X1**2-V3z*V4y*X2**2
+           +V3z*V4y*Y1**2-V3z*V4y*Y2**2+V3z*V4y*Z1**2-V3z*V4y*Z2**2)/
+           (V2x*V3y*V4z-V2x*V3z*V4y-V2y*V3x*V4z+V2y*V3z*V4x+V2z*V3x*V4y-V2z*V3y*V4x))
 
+Y = (0.5*(D1**2*V2x*V3z-D1**2*V2x*V4z-D1**2*V2z*V3x+D1**2*V2z*V4x
+          +D1**2*V3x*V4z-D1**2*V3z*V4x-D2**2*V3x*V4z+D2**2*V3z*V4x
+          +D3**2*V2x*V4z-D3**2*V2z*V4x-D4**2*V2x*V3z+D4**2*V2z*V3x
+          -V2x*V3z*X1**2+V2x*V3z*X4**2-V2x*V3z*Y1**2+V2x*V3z*Y4**2
+          -V2x*V3z*Z1**2+V2x*V3z*Z4**2+V2x*V4z*X1**2-V2x*V4z*X3**2
+          +V2x*V4z*Y1**2-V2x*V4z*Y3**2+V2x*V4z*Z1**2-V2x*V4z*Z3**2
+          +V2z*V3x*X1**2-V2z*V3x*X4**2+V2z*V3x*Y1**2-V2z*V3x*Y4**2
+          +V2z*V3x*Z1**2-V2z*V3x*Z4**2-V2z*V4x*X1**2+V2z*V4x*X3**2
+          -V2z*V4x*Y1**2+V2z*V4x*Y3**2-V2z*V4x*Z1**2+V2z*V4x*Z3**2
+          -V3x*V4z*X1**2+V3x*V4z*X2**2-V3x*V4z*Y1**2+V3x*V4z*Y2**2
+          -V3x*V4z*Z1**2+V3x*V4z*Z2**2+V3z*V4x*X1**2-V3z*V4x*X2**2
+          +V3z*V4x*Y1**2-V3z*V4x*Y2**2+V3z*V4x*Z1**2-V3z*V4x*Z2**2)/
+          (V2x*V3y*V4z-V2x*V3z*V4y-V2y*V3x*V4z+V2y*V3z*V4x+V2z*V3x*V4y-V2z*V3y*V4x))
 
+Z = (-0.5*(D1**2*V2x*V3y-D1**2*V2x*V4y-D1**2*V2y*V3x+D1**2*V2y*V4x
+           +D1**2*V3x*V4y-D1**2*V3y*V4x-D2**2*V3x*V4y+D2**2*V3y*V4x
+           +D3**2*V2x*V4y-D3**2*V2y*V4x-D4**2*V2x*V3y+D4**2*V2y*V3x
+           -V2x*V3y*X1**2+V2x*V3y*X4**2-V2x*V3y*Y1**2+V2x*V3y*Y4**2
+           -V2x*V3y*Z1**2+V2x*V3y*Z4**2+V2x*V4y*X1**2-V2x*V4y*X3**2
+           +V2x*V4y*Y1**2-V2x*V4y*Y3**2+V2x*V4y*Z1**2-V2x*V4y*Z3**2
+           +V2y*V3x*X1**2-V2y*V3x*X4**2+V2y*V3x*Y1**2-V2y*V3x*Y4**2
+           +V2y*V3x*Z1**2-V2y*V3x*Z4**2-V2y*V4x*X1**2+V2y*V4x*X3**2
+           -V2y*V4x*Y1**2+V2y*V4x*Y3**2-V2y*V4x*Z1**2+V2y*V4x*Z3**2
+           -V3x*V4y*X1**2+V3x*V4y*X2**2-V3x*V4y*Y1**2+V3x*V4y*Y2**2
+           -V3x*V4y*Z1**2+V3x*V4y*Z2**2+V3y*V4x*X1**2-V3y*V4x*X2**2
+           +V3y*V4x*Y1**2-V3y*V4x*Y2**2+V3y*V4x*Z1**2-V3y*V4x*Z2**2)/
+(V2x*V3y*V4z-V2x*V3z*V4y-V2y*V3x*V4z+V2y*V3z*V4x+V2z*V3x*V4y-V2z*V3y*V4x))
 
+print X
+print Y
+print Z
 
 
 
